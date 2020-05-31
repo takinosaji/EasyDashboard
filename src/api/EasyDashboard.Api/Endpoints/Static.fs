03448b5a02012cb4f23a1e9964a8129cc5adcbd2
TakinosaJi <kostiantyn.chomakov@gmail.com>
2020-05-31 14:02:01 +0300

    Use native suave browseFileHome function for app root handler

--- src/api/EasyDashboard.Api/Endpoints/Static.fs
+++ src/api/EasyDashboard.Api/Endpoints/Static.fs
@@ -6,6 +6,6 @@ open Suave.Operators
 
     let [<Literal>] private IndexFileName = "index.html"
     
-    let EntryPoint homeFolder =GET >=> path "/" >=> Files.file (sprintf "%s\\%s" homeFolder IndexFileName)        
+    let EntryPoint homeFolder = GET >=> path "/" >=> Files.browseFileHome IndexFileName        
     let AssetFiles = GET >=> Files.browseHome        
     let NotFoundHandler = RequestErrors.NOT_FOUND "Page not found."
