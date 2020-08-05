e24950738f54f7fb8538896d156747b8c5bd340a
TakinosaJi <kostiantyn.chomakov@gmail.com>
2020-08-05 18:48:29 +0300

    Remove unused code
    Add launchSettings.json

--- src/api/EasyDashboard.Host/Program.fs
+++ src/api/EasyDashboard.Host/Program.fs
@@ -9,12 +9,6 @@ module Host =
     let configureAppServices (_:HostBuilderContext) (services:IServiceCollection) =
         services.AddHostedService<WorkerService>() |> ignore<IServiceCollection>
         
-    let configureAppConfiguration (hostContext:HostBuilderContext) (config:IConfigurationBuilder) =  
-        config
-            .AddJsonFile("appsettings.json", false, true)
-            .AddJsonFile(sprintf "appsettings.%s.json" hostContext.HostingEnvironment.EnvironmentName, true)
-            .AddEnvironmentVariables() |> ignore
-        
     let CreateHostBuilder argv : IHostBuilder =
         let builder = Host.CreateDefaultBuilder(argv)
         builder
